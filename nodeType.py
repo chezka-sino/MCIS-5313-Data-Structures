66e977453f8f4f9e6c50615d9cef33fe011eef95
chezka-sino <chezka@pm.me>
2019-10-04 12:39:19 -0700

    Add unsorted build list function

--- nodeType.py
+++ nodeType.py
@@ -11,26 +11,51 @@ class LinkedList:
         self.size = 0
 
     def buildList(self, new_node):
-
+        # Inserts new node at the end of the linkedlist
         currNode = self.head
 
         if currNode is None:
             self.head = new_node
+            self.size += 1
 
         else:
+            while currNode is not None:
+                tempNode = currNode
+                currNode = currNode.link
+
+            tempNode.link = new_node
+            self.size += 1
+
+    def buildList_sorted(self, new_node):
+        # Inserts new node in a sorted way
+
+        currNode = self.head
+
+        if currNode is None:
+            #sets the newnode as the head if linkedlist is empty
+            self.head = new_node
+            self.size += 1
+
+        else:
+            # sets new node as the head if smaller than current head
             if self.head.info > new_node.info:
                 new_node.link = self.head
                 self.head = new_node
                 self.size += 1
-                return
             else:
+                # traverses to through the linkedlist and inserts the new
+                # node accordingly
                 while currNode is not None:
+                    # inserts at the end of the list
                     if currNode.link is None:
                         currNode.link = new_node
                         self.size += 1
                         return
 
                     else:
+                        # compares info of current node to new node and inserts
+                        # new node if applicable
+                        # exits function if node is inserted successfully
                         if currNode.link.info > new_node.info:
                             temp_link = currNode.link
                             currNode.link = new_node
@@ -38,10 +63,13 @@ class LinkedList:
                             self.size += 1
                             return
 
+                    # moves current node to the next on the linkedlist if new node
+                    # is not inserted yet
                     currNode = currNode.link
 
 
     def traverse(self):
+        # traverses through the linked list
         currNode = self.head
 
         if currNode is None:
--- test_linkedlist.py
+++ test_linkedlist.py
@@ -1,12 +1,35 @@
 from nodeType import *
 
-if __name__ == "__main__":
-
+def unsorted_linkedlist(list_num):
     new_linkedlist = LinkedList()
-    list_num = [17, 19, 63, 44]
 
     for item in list_num:
         new_node = nodeType(item)
         new_linkedlist.buildList(new_node)
 
-    new_linkedlist.traverse()
+    return new_linkedlist
+
+def sorted_linkedlist(list_num):
+    new_linkedlist = LinkedList()
+
+    for item in list_num:
+        new_node = nodeType(item)
+        new_linkedlist.buildList_sorted(new_node)
+
+    return new_linkedlist
+
+if __name__ == "__main__":
+
+    list_num = [17, 19, 63, 44]
+
+    unsorted = unsorted_linkedlist(list_num)
+    sorted = sorted_linkedlist(list_num)
+
+    print("UNSORTED LINKED LIST SAMPLE")
+    print("LinkedList Size:", unsorted.size)
+    unsorted.traverse()
+    print()
+
+    print("SORTED LINKED LIST SAMPLE")
+    print("LinkedList Size:", unsorted.size)
+    sorted.traverse()
