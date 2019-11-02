726e8f5ca7bce2fcbe118cee6f9fe473c008175e
chezka.sino <chezka@pm.me>
2019-11-02 00:00:56 -0700

    postfix to infix working

--- Q1/Proj1Q1.py
+++ Q1/Proj1Q1.py
@@ -1,30 +1,47 @@
 def isOperator(symbol):
-    return
+    operators = ['+', '-', '*', '/']
+    return symbol in operators
 
-def reverse(expression):
+
+def reverseExpression(expression):
     expression = expression[len(expression)::-1]
-    print(expression)
-    return
+    return [symbol for symbol in expression]
+
 
 def postfixToInfix(expression):
-    expression = expression[len(expression)::-1]
-    print(expression)
-    # postfix = self.expression.reverse()
+
+    postfix = reverseExpression(expression)
     infix = []
 
-    # print(postfix)
-    return infix
+    while len(postfix) > 0:
+        symbol = postfix.pop()
+
+        if not isOperator(symbol):
+            infix.append(symbol)
+
+        else:
+            val1 = infix.pop()
+            val2 = infix.pop()
+            new_val = '(' + val2 + symbol + val1 + ')'
+            infix.append(new_val)
+
+    return ''.join(infix)
+
 
 def testPostfixToInfix(expression, expected):
     print("Postfix expression:", expression)
     print("Expected Infix:", expected)
-    print("Infix result: TODO")
-
+    print("Infix result:", postfixToInfix(expression))
     return
 
 
 if __name__ == "__main__":
     test_expression = 'ABC*+DE/F*-'
-    test_expected = '(A+(B*C))+((D/E)*F))'
+    test_expected = '(A+(B*C))-((D/E)*F))'
 
     testPostfixToInfix(test_expression, test_expected)
+    print()
+
+    test_expression_2 = 'AB*CD/+'
+    test_expected_2 = '((A*B)+(C/D))'
+    testPostfixToInfix(test_expression_2, test_expected_2)
