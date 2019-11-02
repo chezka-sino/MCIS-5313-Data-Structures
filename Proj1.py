1aef0f372bdc9d070a8a8973b9301b88b2052527
chezka.sino <chezka@pm.me>
2019-11-01 22:34:49 -0700

    initialize proj1

---
+++ Proj1.py
@@ -0,0 +1,16 @@
+class Stack:
+
+    def __init__(self, expression):
+        self.expression = expression
+
+    def isOperator(self, symbol):
+        return
+
+    def reverse(self,expression):
+        return
+
+    def postfixToInfix(self):
+        return
+
+    def infixToPostfix(self):
+        return
