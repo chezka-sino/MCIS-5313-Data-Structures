b0a2020ccb848752822380f9523836bcb2140c31
chezka.sino <chezka@pm.me>
2019-11-04 08:11:19 -0800

    edits

--- Q2/Proj1Q2.py
+++ Q2/Proj1Q2.py
@@ -1,5 +1,5 @@
 def isOperator(symbol):
-    operators = ['+', '-', '*', '/', '(', ')']
+    operators = ['+', '-', '*', '/']
     return symbol in operators
 
 def isOperand(symbol):
@@ -13,12 +13,10 @@ def isRightParenthesis(symbol):
 
 def reverseExpression(expression):
     expression = '(' + expression + ')'
-    print(expression)
     expression = expression[len(expression)::-1]
     return [symbol for symbol in expression]
 
 def hierarchy(symbol):
-    # TODO hierarchy check for operands
     switcher = {
         '*': 2,
         '/': 2,
@@ -27,6 +25,16 @@ def hierarchy(symbol):
     }
 
     return switcher.get(symbol)
+
+def leftToRight(stackOperator, newOperator):
+    if newOperator == '/' and stackOperator == '*':
+        return False
+    elif newOperator == '*' and stackOperator == '/':
+        return True
+    elif newOperator == '-' and stackOperator == '+':
+        return True
+    elif newOperator == '+' and stackOperator == '-':
+        return False
 
 def infixToPostfix(expression):
     # TODO infix to postfix converter
@@ -51,7 +59,26 @@ def infixToPostfix(expression):
                 topOperator = operatorStack.pop()
 
         if isOperator(symbol):
-            print('operator', symbol)
+            topOperator = operatorStack.pop()
+
+            if not isLeftParenthesis(topOperator):
+                if hierarchy(symbol) > hierarchy(topOperator):
+                    operatorStack.append(topOperator)
+                    operatorStack.append(symbol)
+
+                elif hierarchy(symbol) == hierarchy(topOperator):
+                    if leftToRight(topOperator, symbol):
+                        postfix.append(topOperator)
+                        operatorStack.append(symbol)
+                    else:
+                        operatorStack.append(topOperator)
+                        operatorStack.append(symbol)
+
+
+            else:
+                operatorStack.append(topOperator)
+                operatorStack.append(symbol)
+
             print(operatorStack)
 
     return ''.join(postfix)
@@ -63,8 +90,12 @@ def testInfixToPostfix(expression, expected):
     return
 
 if __name__ == "__main__":
-    test_expression = "(A+(B*C))+((D/E)*F)"
-    test_expected = ""
+    test_expression = "(A+(B*C))-((D/E)*F)"
+    test_expected = "ABC*+DE/F*-"
     testInfixToPostfix(test_expression, test_expected)
-    # print(hierarchy('-') <= hierarchy('+'))
-    # print(isLeftParenthesis('('))
+
+    print()
+
+    test_expression2 = "A*(B+C*D)+E"
+    test_expected2 = "ABCD*+*E+"
+    testInfixToPostfix(test_expression2, test_expected2)
