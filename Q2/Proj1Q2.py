654c80f3b29668b1b25f1bf77d65720affa1a155
chezka-sino <chezka@pm.me>
2019-11-03 20:11:43 -0800

    infix to postfix edit

--- Q2/Proj1Q2.py
+++ Q2/Proj1Q2.py
@@ -1,13 +1,60 @@
 def isOperator(symbol):
-    operators = ['+', '-', '*', '/']
+    operators = ['+', '-', '*', '/', '(', ')']
     return symbol in operators
 
-def isParenthesis(symbol):
-    par = ['(',')']
-    return symbol in par
+def isOperand(symbol):
+    return symbol.isalpha()
+
+def isLeftParenthesis(symbol):
+    return symbol == '('
+
+def isRightParenthesis(symbol):
+    return symbol == ')'
+
+def reverseExpression(expression):
+    expression = '(' + expression + ')'
+    print(expression)
+    expression = expression[len(expression)::-1]
+    return [symbol for symbol in expression]
+
+def hierarchy(symbol):
+    # TODO hierarchy check for operands
+    switcher = {
+        '*': 2,
+        '/': 2,
+        '+': 1,
+        '-': 1
+    }
+
+    return switcher.get(symbol)
 
 def infixToPostfix(expression):
-    return
+    # TODO infix to postfix converter
+    infix = reverseExpression(expression)
+    operatorStack = []
+    postfix = []
+
+    while len(infix) > 0:
+        symbol = infix.pop()
+
+        if isOperand(symbol):
+            postfix.append(symbol)
+
+        if isLeftParenthesis(symbol):
+            operatorStack.append(symbol)
+
+        if isRightParenthesis(symbol):
+            topOperator = operatorStack.pop()
+
+            while not isLeftParenthesis(topOperator):
+                postfix.append(topOperator)
+                topOperator = operatorStack.pop()
+
+        if isOperator(symbol):
+            print('operator', symbol)
+            print(operatorStack)
+
+    return ''.join(postfix)
 
 def testInfixToPostfix(expression, expected):
     print("Infix expression:", expression)
@@ -16,6 +63,8 @@ def testInfixToPostfix(expression, expected):
     return
 
 if __name__ == "__main__":
-    test_expression = ""
+    test_expression = "(A+(B*C))+((D/E)*F)"
     test_expected = ""
     testInfixToPostfix(test_expression, test_expected)
+    # print(hierarchy('-') <= hierarchy('+'))
+    # print(isLeftParenthesis('('))
